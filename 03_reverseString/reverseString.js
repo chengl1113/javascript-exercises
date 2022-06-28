8d2a9e4b1fa9227e3ad4972568d4d76431aeee95
cheng <chengl@bu.edu>
2022-06-27 20:32:54 -0400

    completed reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let len = str.length-1;
+    let res = '';
+    for (len; len >= 0; len--){
+        res += str[len];
+    }
+    return res;
 };
 
 // Do not edit below this line
