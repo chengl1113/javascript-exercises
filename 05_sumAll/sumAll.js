ebd4548d0c9d319ab982e3bd035f8d2ca979e3bd
cheng <chengl@bu.edu>
2022-06-27 23:24:30 -0400

    complete sumAll.js and leapYears.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    res = 0;
+    if (x < 0 || y < 0){
+        return 'ERROR';
+    }
 
+    if (typeof x != 'number' || typeof y != 'number'){
+        return 'ERROR';
+    }
+
+    if (x > y){
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for (x; x < y + 1; x++){
+        res += x;
+    }
+    return res;
 };
 
 // Do not edit below this line
